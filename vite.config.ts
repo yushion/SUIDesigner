43b7bc98f30f883a973d2023b2a224a98782fb11
yushion <97763838@QQ.com>
2026-06-07 01:23:06 +0800

    首次发布

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { fileURLToPath, URL } from 'node:url'
 const monacoEditorPlugin = (_monacoEditorPlugin as any).default || _monacoEditorPlugin
 
 export default defineConfig({
+  // GitHub Pages 部署时改为：'/SUIDesigner/'，本地开发改回 '/'
+  base: '/SUIDesigner/',
   plugins: [
     vue(),
     monacoEditorPlugin({
